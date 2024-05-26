3faa40b94a7feb3f838a80eab84b32c5776a0bdb
Rahek <raheksalem@gmail.com>
2024-05-26 16:28:35 +0300

    create project

---
+++ TravelSite/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from "./sign-up/sign-up.component";
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, LoginComponent, SignUpComponent,ForgetPasswordComponent]
+})
+export class AppComponent {
+  title = 'TravelSite';
+}

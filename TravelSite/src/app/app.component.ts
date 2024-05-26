47e5005b6fb16a54bb7726922c337c7bfba15b8a
Rahek <raheksalem@gmail.com>
2024-05-26 18:01:10 +0300

    reset password UI

--- TravelSite/src/app/app.component.ts
+++ TravelSite/src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from "./sign-up/sign-up.component";
 import { ForgetPasswordComponent } from './forget-password/forget-password.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, LoginComponent, SignUpComponent,ForgetPasswordComponent]
+    imports: [RouterOutlet, LoginComponent, SignUpComponent,ForgetPasswordComponent,ResetPasswordComponent]
 })
 export class AppComponent {
   title = 'TravelSite';

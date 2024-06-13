d326cc54c6b581956d872210fa128d4737c7b902
nada ali <nadaelhabashy19@gmail.com>
2024-06-13 07:07:58 +0300

    create home, navbar, footer component

--- TravelSite/src/app/app.component.ts
+++ TravelSite/src/app/app.component.ts
@@ -5,13 +5,18 @@ import { SignUpComponent } from "./sign-up/sign-up.component";
 import { ForgetPasswordComponent } from './forget-password/forget-password.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
 import { ContactUSComponent } from './contact-us/contact-us.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, LoginComponent, SignUpComponent,ForgetPasswordComponent,ResetPasswordComponent,ContactUSComponent]
+    imports: [RouterOutlet, LoginComponent, SignUpComponent,ForgetPasswordComponent
+      ,ResetPasswordComponent,ContactUSComponent
+      ,HomeComponent,NavbarComponent,FooterComponent]
 })
 export class AppComponent {
   title = 'TravelSite';
--- TravelSite/src/app/app.routes.ts
+++ TravelSite/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'', redirectTo: 'search', pathMatch:'full'
+  },
+  {
+    path:'home', component:HomeComponent, title:'home'
+  }
+];

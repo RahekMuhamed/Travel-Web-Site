178f41f33745aac42ee20f389958b4f322193949
Rahek <raheksalem@gmail.com>
2024-06-15 04:18:01 +0300

    getAll fuction

--- TravelSite/src/app/services/services.service.ts
+++ TravelSite/src/app/services/services.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Services } from '../models/services';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicesService {
 
-  constructor() { }
+  private baseUrl: string = "https://localhost:44331/api/Service/";
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Services[]> {
+    return this.http.get<Services[]>(this.baseUrl);
+  }
 }

615e116cdcf2255376e272b0e4a47e3736129730
nada ali <nadaelhabashy19@gmail.com>
2024-06-24 02:47:54 +0300

    adding some components and services

--- TravelSite/src/app/app.routes.ts
+++ TravelSite/src/app/app.routes.ts
@@ -1,12 +1,16 @@
 import { Routes } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { BookingServiceComponent } from './booking-service/booking-service.component';
 
 export const routes: Routes = [
   {
-    path:'', redirectTo: 'search', pathMatch:'full'
+    path:'', redirectTo: 'home', pathMatch:'full'
   },
   {
     path:'home', component:HomeComponent, title:'home'
   }
+ ,
+  { path: 'booking', component: BookingServiceComponent },
+  
 ];
--- TravelSite/src/app/home/home.component.ts
+++ TravelSite/src/app/home/home.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
+import { ApiServiceService } from '../services/api-service.service';
+import { Service } from '../models/service.model';
+import { Router, RouterLink, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent],
+  imports: [
+    NavbarComponent,
+    FooterComponent,
+    CardModule,
+    ButtonModule,
+    CommonModule,
+    RouterModule,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  services: Service[] = [];
 
+  constructor(private apiService: ApiServiceService) {}
+
+  ngOnInit(): void {
+    // this.apiService.getServices().subscribe((data) => {
+    //   this.services = data;
+    // });
+  }
 }

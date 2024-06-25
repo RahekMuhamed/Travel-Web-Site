a9a6667d57a8546c6ea70d028d676354eb779f44
Rahek <raheksalem@gmail.com>
2024-06-25 11:36:19 +0300

    get user info(كروكي لسه)

---
+++ TravelSite/src/app/user-info/user-info.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user';
+import { UserServiceService } from '../services/user-service.service';
+import { CommonModule } from '@angular/common';
+import { AuthServiceService } from '../services/auth-service.service';
+
+@Component({
+  selector: 'app-user-info',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './user-info.component.html',
+  styleUrls: ['./user-info.component.css']  // Fix typo: styleUrl -> styleUrls
+})
+export class UserInfoComponent implements OnInit {
+  user: any;
+  errorMessage: string = '';  // Initialize errorMessage to an empty string
+
+  constructor(private userService: UserServiceService, private authService: AuthServiceService) {}
+
+  ngOnInit() {
+    const token = this.authService.getToken();
+
+    if (token) {
+      const decodedToken = this.authService.decodeToken(token);
+      const userId = decodedToken ? decodedToken['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier'] : null;
+
+      if (userId) {
+        console.log(`Fetching user data for ID: ${userId}`);
+        this.userService.getUserData(userId).subscribe(
+          data => this.user = data,
+          error => this.errorMessage = `Error fetching user data: ${error}`
+        );
+      } else {
+        this.errorMessage = 'User ID is undefined.';
+      }
+    } else {
+      this.errorMessage = 'Token is null.';
+    }
+  }
+}

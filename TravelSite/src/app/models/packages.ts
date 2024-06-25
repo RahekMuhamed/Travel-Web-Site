e3cdf736ea54cbc8f3f8fee295597fdaf16046e8
Rahek <raheksalem@gmail.com>
2024-06-25 11:46:12 +0300

    package model

--- TravelSite/src/app/models/packages.ts
+++ TravelSite/src/app/models/packages.ts
@@ -4,7 +4,7 @@ export class Package {
     public name: string,
     public isDeleted: boolean,
     public description?: string,
-    public image?: string,
+    public image?: string ,
     public quantityAvailable?: number,
     public price?: number,
     public startDate?: Date,

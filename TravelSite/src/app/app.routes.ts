9a59606ee99ce281e7b26c83531c89dcda905ae7
Rahek <raheksalem@gmail.com>
2024-07-02 11:15:49 +0300

    deleted

--- TravelSite/src/app/app.routes.ts
+++ TravelSite/src/app/app.routes.ts
@@ -1,75 +1,152 @@
-import { Routes } from '@angular/router';
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
-import { HomeComponent } from './home/home.component';
-import { BookingServiceComponent } from './booking-service/booking-service.component';
 import { TravelServiceComponent } from './travel-service/travel-service.component';
-import { PackagesComponent } from './packages/packages.component';
-import { ServiceDetailsComponent } from './service-details/service-details.component';
-import { PackageDetailsComponent } from './package-details/package-details.component';
-import { AdminPackagesComponent } from '../../Admin/admin-packages/admin-packages.component';
-import { AddPackageComponent } from '../../Admin/add-package/add-package.component';
-import { PackageDetailsComponent } from '../../Admin/package-details/package-details.component';
-import { EditPackageComponent } from '../../Admin/edit-package/edit-package.component';
-import { SuperpackageDetailsComponent } from '../../SuperAdmin/superpackage-details/superpackage-details.component';
-import { PackagesListComponent } from '../../SuperAdmin/packages-list/packages-list.component';
-import { AdminServicesComponent } from '../../Admin/admin-services/admin-services.component';
-import { EditServiceComponent } from '../../Admin/edit-service/edit-service.component';
-import { ServiceDetailsComponent } from '../../Admin/service-details/service-details.component';
-import { AddServiceComponent } from '../../Admin/add-service/add-service.component';
-import { AdminInfoComponent } from '../../Admin/admin-info/admin-info.component';
-import { LoginComponent } from './login/login.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { UserInfoComponent } from './user-info/user-info.component';
-import { AdminLayoutComponent } from '../../Admin/admin-layout/admin-layout.component';
+import { AdminPackagesComponent } from '../../Dashboard/Admin/admin-packages/admin-packages.component';
+import { AddPackageComponent } from '../../Dashboard/Admin/add-package/add-package.component';
+import { EditPackageComponent } from '../../Dashboard/Admin/edit-package/edit-package.component';
+import { SuperpackageDetailsComponent } from '../../Dashboard/SuperAdmin/superpackage-details/superpackage-details.component';
+import { PackagesListComponent } from '../../Dashboard/SuperAdmin/SuperPackages-list/packages-list.component';
+import { AdminServicesComponent } from '../../Dashboard/Admin/admin-services/admin-services.component';
+import { EditServiceComponent } from '../../Dashboard/Admin/edit-service/edit-service.component';
+import { AddServiceComponent } from '../../Dashboard/Admin/add-service/add-service.component';
+import { SignUpComponent } from '../../Authenticaion/sign-up/sign-up.component';
+import { UserInfoComponent } from '../../Dashboard/user-info/user-info.component';
+import { EditUserInfoComponent } from '../../Dashboard/edit-user-info/edit-user-info.component';
+import { LoginComponent } from '../../Authenticaion/login/login.component';
+import { AddAdminComponent } from '../../Dashboard/SuperAdmin/add-admin/add-admin.component';
+import { PackageDetailsComponent } from '../../Dashboard/Admin/package-details/package-details.component';
+import { CustomersListComponent } from '../../Dashboard/SuperAdmin/customers-list/customers-list.component';
+import { ServiceDetailsComponent } from '../../Dashboard/Admin/service-details/service-details.component';
+import { ServiceProviderDetailComponent } from '../../Dashboard/SuperAdmin/service-provider-detail/service-provider-detail.component';
+import { EditServiceProviderComponent } from '../../Dashboard/SuperAdmin/edit-service-provider/edit-service-provider.component';
+import { ServiceProviderlistComponent } from '../../Dashboard/SuperAdmin/service-providerlist/service-providerlist.component';
+import { AddServiceProviderComponent } from '../../Dashboard/SuperAdmin/add-service-provider/add-service-provider.component';
+import { AuthGuardService } from './services/auth-guard.service';
+import { ServicesListComponent } from '../../Dashboard/SuperAdmin/SuperServices-list/services-list.component';
+import { AdminLayoutComponent } from '../../Dashboard/layout/admin-layout.component';
+import { AdminsListComponent } from '../../Dashboard/SuperAdmin/admins-list/admins-list.component';
+import { SuperserviceDetailsComponent } from '../../Dashboard/SuperAdmin/superservice-details/superservice-details.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
-  {
-    path: 'home',
-    component: HomeComponent,
-    title: 'home',
-  },
-  { path: 'booking', component: BookingServiceComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'services', component: TravelServiceComponent },
-  { path: 'packages', component: PackagesComponent },
-  { path: 'serviceDetails/:id', component: ServiceDetailsComponent },
-    { path: 'serviceDetails/:id', component: PackageDetailsComponent },
-
-  {
-    path: 'SuperAdmin',
-    loadChildren: () =>
-      import('../../SuperAdmin/SuperAdmin.routes').then(
-        (m) => m.SuperAdminroutes
-      ),
-  },
   { path: 'Admin/AddPackage', component: AddPackageComponent },
   { path: 'Admin/AddService', component: AddServiceComponent },
-  // { path: 'Admin/Packagelist', component: AdminPackagesComponent },
-  // { path: 'Admin/servicelist', component: AdminServicesComponent },
-  {path:'Admin/packageDetail/:id',component:PackageDetailsComponent},
-  // {path:'Admin/serviceDetail/:id',component:ServiceDetailsComponent},
+  { path: 'Admin/packageDetail/:id', component: PackageDetailsComponent },
   { path: 'Admin/updatePackage/:id', component: EditPackageComponent },
   { path: 'Admin/updateservice/:id', component: EditServiceComponent },
-  {path :'SuperAdmin/packageDetail/:id',component:SuperpackageDetailsComponent},
-  {path:'SuperAdmin/Packagelist',component:PackagesListComponent},
-  {path: 'Admin/info',component:AdminInfoComponent},
-  {path :'login',component:LoginComponent},
-  {path:'signup',component:SignUpComponent},
-  // {path: 'userinfo',component:UserInfoComponent},
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  // {path:'Aminlayout',component:AdminLayoutComponent},
-  {path: 'Admin',
+
+  { path: 'SuperAdmin/Packagelist', component: PackagesListComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignUpComponent },
+  {
+    path: 'Admin',
     component: AdminLayoutComponent,
     children: [
-      { path: 'userinfo', component: UserInfoComponent },
-      { path: 'servicelist', component: AdminServicesComponent,       },
-      { path: 'packagelist', component: AdminPackagesComponent },
-      {path:'serviceDetail/:id',component:ServiceDetailsComponent},
-
-    ]
-    }
+      {
+        path: 'userinfo',
+        component: UserInfoComponent,
+        canActivate: [AuthGuardService],
+        data: { roles: ['customerService', 'client', 'admin', 'superAdmin'] },
+      },
+      {
+        path: 'EditUserInfo',
+        component: EditUserInfoComponent,
+        canActivate: [AuthGuardService],
+        data: { roles: ['customerService', 'client', 'admin', 'superAdmin'] },
+      },
+      {
+        path: 'servicelist',
+        component: AdminServicesComponent,
+        canActivate: [AuthGuardService],
+        data: { roles: ['admin'] },
+      },
+      {
+        path: 'superservicelist',
+        component: ServicesListComponent,
+        canActivate: [AuthGuardService],
+        data: { roles: ['superAdmin'] },
+      },
+      {
+        path: 'Packagelist',
+        component: AdminPackagesComponent,
+        canActivate: [AuthGuardService],
+        data: { roles: ['admin'] },
+      },
+      {
+        path: 'serviceDetail/:id',
+        component: SuperserviceDetailsComponent,
+        canActivate: [AuthGuardService],
+        data: { roles: ['superAdmin'] },
+      },
+      {
+        path: 'superPackagelist',
+        component: PackagesListComponent,
+        canActivate: [AuthGuardService],
+        data: { roles: ['superAdmin'] },
+      },
+      {
+        path: 'superpackageDetail/:id',
+        component: SuperpackageDetailsComponent,
+        canActivate: [AuthGuardService],
+        data: { roles: ['superAdmin'] },
+      },
+      {
+        path: 'serviceDetail/:id',
+        component: ServiceDetailsComponent,
+        canActivate: [AuthGuardService],
+        data: { roles: ['admin'] },
+      },
+      {
+        path: 'adminslist',
+        component: AdminsListComponent,
+        canActivate: [AuthGuardService],
+        data: { roles: ['superAdmin'] },
+      },
+      {
+        path: 'Customers',
+        component: CustomersListComponent,
+        canActivate: [AuthGuardService],
+        data: { roles: ['superAdmin'] },
+      },
+      {
+        path: 'ServiceProvider',
+        component: ServiceProviderlistComponent,
+        canActivate: [AuthGuardService],
+        data: { roles: ['superAdmin'] },
+      },
+      {
+        path: 'AddAdmin',
+        component: AddAdminComponent,
+        canActivate: [AuthGuardService],
+        data: { roles: ['superAdmin'] },
+      },
+      {
+        path: 'packageDetail/:id',
+        component: PackageDetailsComponent,
+        canActivate: [AuthGuardService],
+        data: { roles: ['admin'] },
+      },
+      {
+        path: 'ServiceProviderDetail/:id',
+        component: ServiceProviderDetailComponent,
+        canActivate: [AuthGuardService],
+        data: { roles: ['superAdmin'] },
+      },
+      {
+        path: 'updateServiceProvider/:id',
+        component: EditServiceProviderComponent,
+        canActivate: [AuthGuardService],
+        data: { roles: ['superAdmin'] },
+      },
+      {
+        path: 'AddServiceProvider',
+        component: AddServiceProviderComponent,
+        canActivate: [AuthGuardService],
+        data: { roles: ['superAdmin'] },
+      },
+    ],
+  },
 ];
